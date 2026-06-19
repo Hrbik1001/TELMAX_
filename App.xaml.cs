546ca0bb08069d63a4e0d6f30db2d7cf011bf5ef
Hrbík <jakubfairaisl@gmail.com>
2026-06-19 19:53:59 +0200

    Fix MAUI startup window

--- App.xaml.cs
+++ App.xaml.cs
@@ -2,15 +2,23 @@ namespace PIDMobileSpeaker;
 
 public partial class App : Application
 {
+    private readonly MainPage _page;
+
     public App(MainPage page)
     {
         InitializeComponent();
-
         UserAppTheme = AppTheme.Dark;
-        MainPage = new NavigationPage(page)
+        _page = page;
+    }
+
+    protected override Window CreateWindow(IActivationState? activationState)
+    {
+        var navigationPage = new NavigationPage(_page)
         {
             BarBackgroundColor = Color.FromArgb("#111820"),
             BarTextColor = Colors.White
         };
+
+        return new Window(navigationPage);
     }
 }
