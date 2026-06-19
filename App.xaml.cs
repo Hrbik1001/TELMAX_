965d4f115ecc3de3937b6297182843702199a320
Hrbík <jakubfairaisl@gmail.com>
2026-06-19 21:29:30 +0200

    Use parameterless app startup

--- App.xaml.cs
+++ App.xaml.cs
@@ -2,23 +2,14 @@ namespace PIDMobileSpeaker;
 
 public partial class App : Application
 {
-    private readonly MainPage _page;
-
-    public App(MainPage page)
+    public App()
     {
         InitializeComponent();
         UserAppTheme = AppTheme.Dark;
-        _page = page;
     }
 
     protected override Window CreateWindow(IActivationState? activationState)
     {
-        var navigationPage = new NavigationPage(_page)
-        {
-            BarBackgroundColor = Color.FromArgb("#111820"),
-            BarTextColor = Colors.White
-        };
-
-        return new Window(navigationPage);
+        return new Window(new MainPage());
     }
 }
